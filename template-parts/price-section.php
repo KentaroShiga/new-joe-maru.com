b471e57f734154f624358c661727e724cb8bac41
KentaroShiga <sugarcane0602@gmail.com>
2026-03-31 09:39:13 +0900

    Update price descriptions and remove pre-3/31 cards

--- template-parts/price-section.php
+++ template-parts/price-section.php
@@ -7,11 +7,6 @@
 $price_page = get_page_by_path('price');
 $price_url = $price_page ? get_permalink($price_page->ID) : home_url('?page_id=3');
 
-// 自動切り替え判定（令和8年4月1日 = 20260401）
-$today = current_time('Ymd');
-$switch_date = '20260401';
-$is_new_season = ($today >= $switch_date);
-
 /**
  * 料金カードを出力するサブ関数
  */
@@ -44,44 +39,8 @@ function render_price_card($plan_id, $title, $sub, $price, $unit, $descriptions,
 
 <section class="toppage-price">
     <div class="price-inner">
-
-        <?php if (!$is_new_season) : ?>
-            <!-- 令和8年3月31日まで -->
-            <div class="price-season-block">
-                <p class="price-season-heading">＼令和8年3/31までの料金となります／</p>
-                <div class="price-title-area">
-                    <h2 class="price-title">料金について</h2>
-                </div>
-                <div class="price-cards">
-                    <?php 
-                    render_price_card('planA', 'カセ・筏釣り', '1名様～', '～¥4,000', '/ 1人', 
-                        ['チヌ・季節に応じて青物・アオリイカなど', '1名様から出船します', '女性・子ども（中学生まで）は半額となります', '釣り時間はお知らせをご確認ください'], 
-                        $price_url, '#planA-detail');
-                    
-                    render_price_card('planB', '近海チャーター船', '2名様～', '～¥12,000', '/ 1人', 
-                        ['ルアー五目便', '2名様から出船します', 'スタンプカード・人数によって割引がございます', '6時間程度'], 
-                        $price_url, '#planB-detail');
-
-                    render_price_card('planC', '湾内チャーター船', '1名様 ～', '¥15,000', '/ 1隻', 
-                        ['ルアー五目便', '3名程度まで乗船可能です', '　', '5時間程度'], 
-                        $price_url, '#planC-detail');
-                    ?>
-                </div>
-            </div>
-
-            <!-- 下矢印演出 -->
-            <div class="price-season-arrow">
-                <svg width="40" height="24" viewBox="0 0 40 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M20 24L0 0H40L20 24Z" fill="#78B96C"/>
-                </svg>
-            </div>
-        <?php endif; ?>
-
-        <!-- 令和8年4月1日より（新料金ブロック） -->
+        <!-- 新料金ブロック -->
         <div class="price-season-block price-season-block--new">
-            <?php if (!$is_new_season) : ?>
-                <p class="price-season-heading">＼令和8年4/1からの料金となります／</p>
-            <?php endif; ?>
             <div class="price-title-area">
                 <h2 class="price-title">料金について</h2>
             </div>
